3fb8cd009905bc851fe045793d4c117762754f3f
戴明亮 <dml1630@163.com>
2018-05-08 21:33:56 +0800

    code

---
+++ MLProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLProject
+//
+//  Created by 戴明亮 on 2018/5/8.
+//  Copyright © 2018年 ML Day. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MLProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MLProject
+//
+//  Created by 戴明亮 on 2018/5/8.
+//  Copyright © 2018年 ML Day. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
